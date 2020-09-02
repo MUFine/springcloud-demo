cd5e9842f7510cc11ea797a1f66d034a5e6f94c6
MHY <mohaiyong@bonc.com.cn>
2020-09-02 19:15:46 +0800

    springcloud的demo的第一次修改

--- demo-client/src/main/java/com/m/demo/controller/TestController.java
+++ demo-client/src/main/java/com/m/demo/controller/TestController.java
@@ -5,6 +5,8 @@ import com.m.demo.common.Message;
 import com.m.demo.entity.ResultData;
 import com.m.demo.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,13 +18,22 @@ import java.util.Map;
  * date:2020/9/1 17:03
  */
 @RestController
+@RefreshScope
 public class TestController {
     @Autowired
     private TestService testService;
+    @Value("${test}")
+    private String data;
+
+    @GetMapping("/test")
+    public String test(){
+        return this.data;
+    }
 
     @GetMapping("/getData")
     public ResultData getData(){
         return new ResultData(Code.SUCCESS_CODE, Message.SUCCESS,testService.getData());
     }
+
 
 }
