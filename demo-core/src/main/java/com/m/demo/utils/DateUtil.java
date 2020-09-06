d5ffa801cee89ef048742561e6d6a4a9c4a2d0bf
MHY <mohaiyong@bonc.com.cn>
2020-09-06 16:52:36 +0800

    springcloud的时间工具类

---
+++ demo-core/src/main/java/com/m/demo/utils/DateUtil.java
@@ -0,0 +1,43 @@
+package com.m.demo.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * author:M
+ * describe:
+ * date:2020/9/6 16:28
+ */
+public class DateUtil {
+    public static Date strToDate(String str,int option){
+        SimpleDateFormat simpleDateFormat = null;
+        Date date = null;
+        if(option == 1){
+            simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        }else {
+            simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        }
+        try {
+            date = simpleDateFormat.parse(str);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+
+    public static String dateToStr(Date date,int option){
+        SimpleDateFormat simpleDateFormat = null;
+        if(option == 1){
+            simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        }else {
+            simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        }
+        String str = simpleDateFormat.format(date);
+        return str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(dateToStr(new Date(), 2));
+    }
+}
